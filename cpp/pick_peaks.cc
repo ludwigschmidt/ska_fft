77f9c97db87bfbfdbdb130d14f567924b104b9c4
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2014-08-30 15:48:50 -0400

    adding program options to pick_peaks

--- cpp/pick_peaks.cc
+++ cpp/pick_peaks.cc
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+#include <unistd.h>
+
 #include "helpers.h"
 
 using namespace std;
@@ -10,29 +12,33 @@ struct region {
   int x1, y1, x2, y2;
 };
 
+struct Options {
+  string data_filename;
+  long long n;
+  string regions_filename;
+};
+
+bool parse_options(Options* options, int argc, char** argv);
 bool read_regions(const char* filename, vector<region>* regions);
 
 int main(int argc, char** argv) {
-  // read program options
-  if (argc != 4) {
-    printf("Wrong number of arguments.\n");
+  Options opts;
+  if (!parse_options(&opts, argc, argv)) {
     return 1;
   }
-  const char* data_filename = argv[1];
-  const int n = atoi(argv[2]);
-  const char* region_filename = argv[3];
 
-  const int n2 = n * n;
+  const long long n = opts.n;
+  const long long n2 = n * n;
 
   vector<complexf> data(n2, 0);
   
-  if (!read_input(data.data(), data_filename, n)) {
+  if (!read_input(data.data(), opts.data_filename.c_str(), n)) {
     return 1;
   }
 
   vector<region> regions;
 
-  if (!read_regions(region_filename, &regions)) {
+  if (!read_regions(opts.regions_filename.c_str(), &regions)) {
     return 1;
   }
   
@@ -86,5 +92,43 @@ bool read_regions(const char* filename, vector<region>* regions) {
   }
 
   fclose(f);
+  return true;
+}
+
+
+bool parse_options(Options* options, int argc, char** argv) {
+  options->data_filename = "";
+  options->regions_filename = "";
+  options->n = -1;
+
+  int c;
+  while ((c = getopt(argc, argv, "i:n:r:")) != -1) {
+    if (c == 'i') {
+      options->data_filename = string(optarg);
+    } else if (c == 'n') {
+      options->n = stoi(string(optarg));
+    } else if (c == 'r') {
+      options->regions_filename = string(optarg);
+    } else {
+      printf("Could not parse command line options.\n");
+      return false;
+    }
+  }
+
+  if (options->data_filename == "") {
+    printf("Data file option not set.\n");
+    return false;
+  }
+
+  if (options->regions_filename == "") {
+    printf("Regions file option not set.\n");
+    return false;
+  }
+
+  if (options->n == -1) {
+    printf("Input size option not set.\n");
+    return false;
+  }
+
   return true;
 }
