20a0774dbeccef3c1ef91a7ce3e8b0160b7029e3
Ludwig Schmidt <ludwigschmidt2@gmail.com>
2014-08-30 15:43:04 -0400

    adding program options to slabfft

--- cpp/slab_fft.cc
+++ cpp/slab_fft.cc
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include <fftw3.h>
+#include <unistd.h>
 
 #include "helpers.h"
 
@@ -16,17 +17,23 @@ using namespace std;
 
 typedef pair<int, int> location;
 
+struct Options {
+  string input_filename;
+  string output_filename;
+  long long n;
+  int num_trials;
+  bool use_diagonal_slabs;
+};
+
+bool parse_options(Options* options, int argc, char** argv);
+
 
 int main(int argc, char** argv) {
-  // read program options
-  if (argc !=  5) {
-    printf("Wrong number of arguments.\n");
+  Options opts;
+  if (!parse_options(&opts, argc, argv)) {
     return 1;
   }
-  const char* filename = argv[1];
-  const long long n = atoi(argv[2]);
-  const char* output_filename = argv[3];
-  const int num_trials = atoi(argv[4]);
+  const long long n = opts.n;
 
   vector<double> running_times;
 
@@ -45,7 +52,7 @@ int main(int argc, char** argv) {
 
   // read input data (frequency domain)
   vector<complexf> data(n2);
-  if (!read_input(data.data(), filename, n)) {
+  if (!read_input(data.data(), opts.input_filename.c_str(), n)) {
     return 1;
   }
 
@@ -85,7 +92,7 @@ int main(int argc, char** argv) {
       fftwf_malloc(sizeof(fftwf_complex) * total_slab_size));
 
 
-  for (int trial = 0; trial < num_trials; ++trial) {
+  for (int trial = 0; trial < opts.num_trials; ++trial) {
     auto overall_start = chrono::high_resolution_clock::now();
 
     // copy in data
@@ -334,8 +341,8 @@ int main(int argc, char** argv) {
   fftwf_free(diaglo_data);
 
 
-  if (strcmp(output_filename, "none") != 0) {
-    if (!write_data(output.data(), output_filename, n)) {
+  if (opts.output_filename != "") {
+    if (!write_data(output.data(), opts.output_filename.c_str(), n)) {
       return 1;
     }
   }
@@ -372,4 +379,43 @@ int main(int argc, char** argv) {
   */
 
   return 0;
+}
+
+
+bool parse_options(Options* options, int argc, char** argv) {
+  options->input_filename = "";
+  options->output_filename = "";
+  options->n = -1;
+  options->num_trials = 1;
+  options->use_diagonal_slabs = false;
+
+  int c;
+  while ((c = getopt(argc, argv, "i:n:o:t:x")) != -1) {
+    if (c == 'i') {
+      options->input_filename = string(optarg);
+    } else if (c == 'n') {
+      options->n = stoi(string(optarg));
+    } else if (c == 'o') {
+      options->output_filename = string(optarg);
+    } else if (c == 't') {
+      options->num_trials = stoi(string(optarg));
+    } else if (c == 'x') {
+      options->use_diagonal_slabs = true;
+    } else {
+      printf("Could not parse command line options.\n");
+      return false;
+    }
+  }
+
+  if (options->input_filename == "") {
+    printf("Input file option not set.\n");
+    return false;
+  }
+
+  if (options->n == -1) {
+    printf("Input size option not set.\n");
+    return false;
+  }
+
+  return true;
 }
